0e78086cb13c3f7c6f431e6c2d93cab191b4f9fd
Sean <shlim4915@gmail.com>
2024-03-17 21:23:51 +0800

    rename: 불필요 폴더 제거, pulbic/assets/images/ico 폴더 이름변경, index.html 내 ico->png  변경

--- constants/hyConstants.js
+++
@@ -1,8 +0,0 @@
-export const SYSTEM_MODE = import.meta.env.VITE_APP_MODE;
-export const CRTYPTO_KEY = import.meta.env.VITE_APP_CRYPTO_KEY;
-export const CRTYPTO_IV = import.meta.env.VITE_APP_CRYPTO_IV;
-
-export const REG_SUCCESS_MSG = '등록되었습니다.';
-export const MOD_SUCCESS_MSG = '수정되었습니다.';
-export const DEL_SUCCESS_MSG = '삭제되었습니다.';
-export const DEFAULT_SUCCESS_MSG = '완료되었습니다.';
--- constants/urlConstants.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = import.meta.env.VITE_APP_BASE_URL; // 로컬 - http://localhost:8080
-export const API_BASE_URL = `${BASE_URL}/api`; // 로컬 - http://localhost:8080/api
