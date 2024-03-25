a3330ff257c7263dce7966665a79546956e417b7
Seanniie <shlim4915@gmail.com>
2024-03-25 18:57:39 +0900

    feat: user svg추가, 로그인 후 헤더 수정 중 집가서 할거 임시 커밋,

--- src/components/layouts/header/HeaderButtonContainer.jsx
+++ src/components/layouts/header/HeaderButtonContainer.jsx
@@ -2,14 +2,74 @@ import styled from 'styled-components';
 import HeaderButton from '@components/ui/Button/HeaderButton';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@enums/CommonEnum';
+import useLoginStore from '@store/useLoginStore';
+import UserIcon from '@assets/images/svg/user.svg?react';
+import SvgComponent from '@components/ui/Logo/SvgComponent';
 
 const HeaderButtonContainer = () => {
   const nav = useNavigate();
+  const isLogin = useLoginStore((state) => state.isLogin);
+
+  // const handleButtonClick = (e) => {
+  //   console.log('click left button', e);
+  // };
+  // const handleMenuClick = (e) => {
+  //   console.log('click', e);
+  // };
+  // const items = [
+  //   {
+  //     label: '1st menu item',
+  //     key: '1',
+  //   },
+  //   {
+  //     label: '2nd menu item',
+  //     key: '2',
+  //   },
+  //   {
+  //     label: '3rd menu item',
+  //     key: '3',
+  //     danger: true,
+  //   },
+  //   {
+  //     label: '4rd menu item',
+  //     key: '4',
+  //     danger: true,
+  //     disabled: true,
+  //   },
+  // ];
+  // const menuProps = {
+  //   items,
+  //   onClick: handleMenuClick,
+  // };
+
+  // const CustomButton = ({ src, width, height }) => (
+  //   <Button icon={<SvgComponent src={src} width={width} height={height} />}>Dropdown</Button>
+  // );
 
   return (
     <ButtonContainer>
-      <HeaderButton type="sub" title="회원가입" onClick={() => nav(ROUTES.LOGIN)} />
-      <HeaderButton type="primary" title="로그인" onClick={() => nav(ROUTES.LOGIN)} />
+      {isLogin ? (
+        <>
+          {/* <Dropdown menu={{ items }}>123</Dropdown> */}
+          <div
+            style={{
+              width: '60px',
+              height: '60px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              cursor: 'pointer',
+            }}
+          >
+            <SvgComponent src={UserIcon} width={24} height={30} />
+          </div>
+        </>
+      ) : (
+        <>
+          <HeaderButton type="sub" title="회원가입" onClick={() => nav(ROUTES.SIGNUP)} />
+          <HeaderButton type="primary" title="로그인" onClick={() => nav(ROUTES.LOGIN)} />
+        </>
+      )}
     </ButtonContainer>
   );
 };
--- src/pages/Sample2/index.jsx
+++ src/pages/Sample2/index.jsx
@@ -1,14 +1,24 @@
+import { ROUTES } from '@enums/CommonEnum';
 import useLoadingStore from '@store/useLoadingStore';
+import useLoginStore from '@store/useLoginStore';
 import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const Sample2 = () => {
   const setLoading = useLoadingStore((state) => state.setLoading);
+  const { setIsLogin } = useLoginStore();
+  const nav = useNavigate();
 
   const handleLoading = () => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
     }, 1000 * 2);
+  };
+
+  const handleLogout = () => {
+    setIsLogin(false);
+    nav(ROUTES.HOME);
   };
 
   return (
@@ -18,6 +28,9 @@ const Sample2 = () => {
       </Button>
       <Button type="primary" onClick={handleLoading}>
         푸쉬알림
+      </Button>
+      <Button type="primary" onClick={handleLogout}>
+        로그아웃
       </Button>
     </div>
   );
