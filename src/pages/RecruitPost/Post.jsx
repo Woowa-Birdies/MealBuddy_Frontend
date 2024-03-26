0cecf34d9a220cf3ed562f53c90f83b70ddc3d28
SH-indeyong <ohsh1115@gmail.com>
2024-03-26 21:24:48 +0900

    feat: 모집글 페이지 레이아웃

---
+++ src/pages/RecruitPost/Post.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Post = () => {
+  return <Container>모집글입니다.</Container>;
+};
+
+export default Post;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
--- src/pages/RecruitPost/index.jsx
+++ src/pages/RecruitPost/index.jsx
@@ -1,6 +1,24 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import UserContainer from '@/pages/RecruitPost/UserContainer';
+import Post from '@/pages/RecruitPost/Post';
+import ButtonContainer from '@/pages/RecruitPost/ButtonContainer';
+
 const RecruitPost = () => {
-  return <div>모집글 페이지입니다.</div>;
+  return (
+    <RecruitPostPage>
+      <UserContainer />
+      <Post />
+      <ButtonContainer />
+    </RecruitPostPage>
+  );
 };
 
 export default RecruitPost;
+
+const RecruitPostPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 100px;
+  padding: 30px;
+  gap: 100px;
+`;
