1adf5faf439c29ce7a415b868a1933984f36eeca
Sean <shlim4915@gmail.com>
2024-04-07 22:04:00 +0800

    interceptor내 로딩모달구현

--- src/api/biz/sampleApi.js
+++ src/api/biz/sampleApi.js
@@ -1,9 +1,19 @@
-import { get } from '@/api/common/apiUtils';
+import { get, post } from '@/api/common/apiUtils';
 
 const getHealthCheck = async () => {
   return get(`/actuator/health`);
 };
 
+const postAsk = async (askRequest) => {
+  return post(`/ask`, askRequest);
+};
+
+const postCreate = async (postCreateDto) => {
+  return post(`/post`, postCreateDto);
+};
+
 export default {
   getHealthCheck,
+  postAsk,
+  postCreate,
 };
