1a5748b081aeb05676edcb49fa94f192a6ddfb41
SH-indeyong <ohsh1115@gmail.com>
2024-04-08 23:40:01 +0900

    feat: 미선택 알림

--- src/pages/Recruit/RecruitCompletedButton.jsx
+++ src/pages/Recruit/RecruitCompletedButton.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Modal } from 'antd';
 import CompletedButton from '@components/ui/Button/CompletedButton';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@enums/CommonEnum';
@@ -8,7 +9,34 @@ const RecruitCompletedButton = () => {
   const nav = useNavigate();
   const { recruitPost } = useRecruitStore();
 
+  const showWarning = (message) => {
+    Modal.warning({
+      content: message,
+      style: { top: '35vh' },
+    });
+  };
+
   const handleClick = () => {
+    // 필수 필드 리스트
+    const requiredFields = [
+      { key: 'foodTypeTag', label: '식사 유형을' },
+      { key: 'place', label: '식사 장소를' },
+      { key: 'meetAt', label: '모임 날짜를' },
+      { key: 'closeAt', label: '모집 마감일을' },
+      { key: 'genderTag', label: '성별을' },
+      { key: 'ageTag', label: '연령대를' },
+    ];
+
+    // 빈 필드 찾기
+    const missingFields = requiredFields.filter((field) => !recruitPost[field.key]);
+
+    // 빈 필드가 있으면 경고 메시지 표시
+    if (missingFields.length > 0) {
+      const missingFieldNames = missingFields[0].label;
+      showWarning(`${missingFieldNames} 선택해주세요`);
+      return;
+    }
+
     console.log(recruitPost);
     nav(ROUTES.RECRUITPOST);
   };
