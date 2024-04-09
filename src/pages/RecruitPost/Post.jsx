5358b460ecc0b10dc00782e5b56f329631127435
SH-indeyong <ohsh1115@gmail.com>
2024-04-10 02:01:09 +0900

    feat: showPostApi 연결

--- src/pages/RecruitPost/Post.jsx
+++ src/pages/RecruitPost/Post.jsx
@@ -4,47 +4,42 @@ import styled from 'styled-components';
 import Map from '@/pages/RecruitPost/Map';
 import StatusButton from '@components/ui/Button/StatusButton';
 import SelectButton from '@components/ui/Button/SelectButton';
-import useRecruitStore from '@store/useRecruitStore';
 import Typography from '@components/ui/Typography/Typography';
 import dayjs from 'dayjs';
 import showPostApi from '@api/biz/showPostApi';
 
 const Post = () => {
-  const { recruitPost } = useRecruitStore();
-  const aa = dayjs(recruitPost.meetAt).format('YYYY년 MM월 DD일 dddd A hh:mm');
-
   const { postId } = useParams(); // 현재 경로에서 postId 파라미터를 추출
-  console.log('아이디는 ', postId);
   const [post, setPost] = useState({});
 
+  const aa = dayjs(post.meetAt).format('YYYY년 MM월 DD일 dddd A hh:mm');
   useEffect(() => {
     const fetchPost = async () => {
       try {
         const res = await showPostApi.showPost({ postId });
-        console.log(res.data);
-        setPost(res);
+        setPost(res.data);
       } catch (error) {
         console.error('Failed to fetch post:', error);
       }
     };
 
     fetchPost();
-  }, [postId, post]); // postId가 변경될 때마다 요청을 다시 보냅니다.
+  }, [postId]);
 
   return (
     <Container>
       <StatusButton title="모집중" />
-      <Typography content={recruitPost.place} size="large" />
+      <Typography content={post.place} size="large" />
       <TagContainer>
-        <SelectButton title={recruitPost.foodTypeTag} type="tag" />
-        <SelectButton title={recruitPost.ageTag} type="tag" />
-        <SelectButton title={recruitPost.genderTag} type="tag" />
+        <SelectButton title={post.foodTypeTag} type="tag" />
+        <SelectButton title={post.ageTag} type="tag" />
+        <SelectButton title={post.genderTag} type="tag" />
       </TagContainer>
       <Info>{aa}</Info>
-      <Info>{recruitPost.participantTotal}명 모집</Info>
-      <Info>{recruitPost.address}</Info>
+      <Info>{post.participantTotal}명 모집</Info>
+      <Info>{post.address}</Info>
       <Map />
-      <Contents>{recruitPost.contents}</Contents>
+      <Contents>{post.contents}</Contents>
     </Container>
   );
 };
