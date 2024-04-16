fd6f1b9623e3e27353efb4ddb0558d97f04d1318
SH-indeyong <ohsh1115@gmail.com>
2024-04-16 21:01:06 +0900

    feat: 필터 아이콘 추가

--- src/pages/Home/List.jsx
+++ src/pages/Home/List.jsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
+import Paragraphy from '@components/ui/Paragraphy/Paragraphy';
+import filter from '@assets/images/svg/filter.svg';
 import PostCard from '@/pages/Home/PostCard';
 
 const List = () => {
   return (
     <Container>
+      <FilterContainer>
+        <Filter src={filter} />
+        <Paragraphy content="필터" size="medium" />
+      </FilterContainer>
       <PostCard />
     </Container>
   );
@@ -13,6 +19,15 @@ export default List;
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   width: 66.67vw;
   gap: 1.04vw;
 `;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 5px;
+`;
+
+const Filter = styled.img``;
