80467568b324f4a1f7be8cbc8c4b5672fe77c3bd
Seanniie <shlim4915@gmail.com>
2024-04-19 17:52:07 +0900

    api 주소 변경

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -12,4 +12,4 @@ export const DEFAULT_SUCCESS_MSG = '완료되었습니다.';
 // export const API_BASE_URL = `${BASE_URL}/api`; // 로컬 - http://localhost:8080/api
 
 export const BASE_URL = import.meta.env.VITE_APP_BASE_URL; // 로컬 - http://localhost:8080
-export const API_BASE_URL = `${BASE_URL}/api`; // 로컬 - http://localhost:8080/api
+export const API_BASE_URL = `${BASE_URL}`; // 로컬 - http://localhost:8080/api
