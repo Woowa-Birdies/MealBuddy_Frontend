f08272555e3648039fefd486b8bbf2743c2d66bd
Sean <shlim4915@gmail.com>
2024-04-20 12:02:07 +0900

    쿠키확인

--- src/components/layouts/Base.jsx
+++ src/components/layouts/Base.jsx
@@ -1,8 +1,26 @@
 import Footer from '@components/layouts/footer';
 import Header from '@components/layouts/header';
+import { useEffect } from 'react';
 import styled from 'styled-components';
+import Cookies from 'js-cookie';
 
 const Base = ({ children }) => {
+  useEffect(() => {
+    // 쿠키 확인
+    const accessCookie = Cookies.get('__Secure-access');
+    const refreshCookie = Cookies.get('__Secure-refresh');
+
+    console.log('accessCookie', accessCookie);
+    console.log('refreshCookie', refreshCookie);
+
+    if (accessCookie && refreshCookie) {
+      console.log('__Secure-access 쿠키가 있습니다:', accessCookie);
+      console.log('__Secure-refresh 쿠키가 있습니다:', refreshCookie);
+    } else {
+      console.log('필요한 쿠키가 존재하지 않습니다.');
+    }
+  }, []);
+
   return (
     <>
       <Header />
