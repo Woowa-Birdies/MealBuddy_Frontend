7bcab29e185014c86e0c453b672188baae412284
SH-indeyong <ohsh1115@gmail.com>
2024-04-22 23:10:14 +0900

    fix: 모집글 선택 초기화

--- src/pages/Recruit/index.jsx
+++ src/pages/Recruit/index.jsx
@@ -1,17 +1,14 @@
-import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import RecruitTitle from '@/pages/Recruit/RecruitTitle';
 import RecruitForm from '@/pages/Recruit/RecruitForm';
 import RecruitCompletedButton from '@/pages/Recruit/RecruitCompletedButton';
 
 const Recruit = () => {
-  const { postId } = useParams();
-
   return (
     <RecruitPage>
       <RecruitTitle />
-      <RecruitForm postId={postId} />
-      <RecruitCompletedButton postId={postId} />
+      <RecruitForm />
+      <RecruitCompletedButton />
     </RecruitPage>
   );
 };
